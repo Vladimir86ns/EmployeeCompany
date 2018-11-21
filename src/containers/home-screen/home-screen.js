22a64b87c135cc62594b218be9e1fe487086d0aa
vladimir86ns <vladimir86ns@gmail.com>
2018-11-21 23:23:47 +0100

    refactor - refactored home page

--- src/containers/home-screen/home-screen.js
+++ src/containers/home-screen/home-screen.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Container, Header, Content, Card, Title, Left, Button, Icon, Right, CardItem, Text, Body } from "native-base";
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  Title,
+  Left,
+  Button,
+  Icon,
+  Right,
+  CardItem,
+  Text,
+  Body,
+  Footer,
+  FooterTab,
+  View
+} from "native-base";
 
 class HomeScreen extends Component {
 
@@ -26,32 +42,94 @@ class HomeScreen extends Component {
         <Header headerStyle={{color: "red"}}>
           <Left>
             <Button transparent onPress={() => alert('opaa')}>
-            <Icon name='menu' />
+              <Icon name='menu' />
             </Button>
           </Left>
-          <Title style={{ fontSize: 20, marginTop: 12 }}>Welcome {this.props.user.first_name}</Title>
+
+          <Title style={{ fontSize: 20, marginTop: 12 }}>Company Name</Title>
+
           <Right>
             <Button transparent>
+            <Icon name='notifications' />
             </Button>
           </Right>
         </Header>
+
         <Content padder>
+
           <Card>
             <CardItem header bordered>
               <Text
               style={{ fontSize: 30 }}
-              >Your login time {currentDate.getHours()}:{currentDate.getMinutes()}h </Text>
+              >Login time {currentDate.getHours()}:{currentDate.getMinutes()}h </Text>
             </CardItem>
+
             <CardItem>
               <Body>
                 <Text>
-                  Welcome to your company!
+                  First Name:  {this.props.user.first_name}
+                </Text>
+                <Text>
+                  Last Name:  {this.props.user.last_name}
+                </Text>
+                <Text>
+                  Employee ID:  {this.props.user.last_name}
+                </Text>
+                <View style={{flexDirection: "row", marginTop: 10}}>
+                  <Button rounded success>
+                    <Text>Accept</Text>
+                  </Button>
+                  <Button rounded warning style={{ marginLeft: 30 }}>
+                    <Text>Make Notice</Text>
+                  </Button>
+                </View>
+              </Body>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered>
+              <Text
+              style={{ fontSize: 30 }}
+              >Orders</Text>
+            </CardItem>
+
+            <CardItem>
+              <Body>
+                <Text>
+                  Today: 0
+                </Text>
+                <Text>
+                  In your shift: 0
                 </Text>
               </Body>
             </CardItem>
           </Card>
         </Content>
+
+        <Footer>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="document" />
+              <Text>Tasks</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="list-box" />
+              <Text>Order List</Text>
+            </Button>
+            <Button vertical>
+              <Icon active name="cart" />
+              <Text>Need to buy</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="megaphone" />
+              <Text>Note</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+
       </Container>
+
     );
   }
 }
